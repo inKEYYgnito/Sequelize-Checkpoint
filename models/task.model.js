2db32d3664ee78ac71b5642608532cea462dab93
Kristina Cayetano <kma.cayetano@gmail.com>
2019-09-07 15:42:42 -0400

    finished sequelize checkpoint

--- models/task.model.js
+++ models/task.model.js
@@ -22,10 +22,47 @@ const Task = db.define('Task', {
   due: Sequelize.DATE
 });
 
+Task.clearCompleted = async () => {
+  await Task.destroy({
+    where: {
+      complete: true
+    }
+  })
+}
 
+Task.completeAll = async () => {
+  await Task.update({
+    complete: true
+  }, {
+    where: {
+      complete: false
+    }
+  })
+}
 
 Task.belongsTo(Task, {as: 'parent'});
 
+Task.prototype.getTimeRemaining = function() {
+  if (!this.due) return Infinity
+  else return this.due - (new Date())
+}
+
+Task.prototype.isOverdue = function() {
+  if (this.complete) return false
+  return !(this.getTimeRemaining() > 0)
+}
+
+Task.prototype.addChild = async function({ name }) {
+  return await Task.create({name, parentId: this.id})
+}
+
+Task.prototype.getChildren = async function() {
+  return await Task.findAll({ where: { parentId: this.id}})
+}
+
+Task.prototype.getSiblings = async function() {
+  return await Task.findAll({ where: { parentId: this.parentId, id: { $ne: this.id }}})
+}
 
 //---------^^^---------  your code above  ---------^^^----------
 
